4be3b27fe46048ca5e9000ff11bb038bb627633e
Mirosław Gucwa <mgucwa@gmail.com>
2022-07-13 09:03:07 +0200

    Add Moongoe

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,94 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const database = require('../db');
-const { v4: uuidv4 } = require('uuid');
+const SeatController = require('../controllers/seats.controller');
 
-// get all
-router.route('/seats').get((req, res) => {
-  const db = database.seats;
-  res.json(db);
-});
-
-// get single or random
-router.route('/seats/:id').get((req, res) => {
-  const db = database.seats;
-
-  if (req.params.id === 'random') {
-    res.json(db[Math.floor(Math.random() * db.length)]);
-  }
-  else {
-    const results = db.find(obj => {
-      return obj.id == req.params.id;
-    })
-    if (results)
-      res.json(results);
-    else
-      res.json({ message: '404: Page not found!' });
-  }
-});
-
-// add 
-router.route('/seats/').post((req, res) => {
-  const db = database.seats;
-  const { day, seat, client, email } = req.body;
-
-  if (day && seat && client && email) {
-
-    const results = db.find(obj => {
-      return (obj.day == day && obj.seat == seat);
-    })
-
-    if (!results) {
-      res.json({ message: 'Save' });
-      const obj = {
-        id: uuidv4(),
-        day: day,
-        seat: seat,
-        client: client,
-        email: email,
-      };
-      db.push(obj);
-
-      // websocket
-      req.io.emit('seatsUpdated', db);
-    }
-    else {
-      res.status(500);
-      res.json({ message: 'The slot is already taken...' });
-      // console.log('The slot is already taken...');
-      // console.log(db);
-    }
-  }
-  else {
-    res.json({ message: 'You can\'t leave fields empty!' });
-  }
-});
-
-// update
-router.route('/seats/:id').put((req, res) => {
-  const db = database.seats;
-  const { day, seat, client, email } = req.body;
-
-  const results = db.find(obj => {
-    return obj.id == req.params.id;
-  })
-
-  results.performer = performer,
-  results.genre = genre,
-  results.price = price,
-  results.day = day,
-  results.image = image,
-
-  res.json({ message: 'Update OK' });
-});
-
-// delete
-router.route('/seats/:id').delete((req, res) => {
-  const db = database.seats;
-  const id = parseInt(req.params.id);
-  const removeIndex = db.map(function (item) { return item.id; }).indexOf(id);
-  if (parseInt(removeIndex) >= 0) db.splice(removeIndex, 1);
-  res.json({ message: 'Delete OK' });
-});
+router.get('/seats', SeatController.getAll);
+router.get('/seats/random', SeatController.getRandom);
+router.get('/seats/:id', SeatController.getById);
+router.post('/seats', SeatController.addOne);
+router.put('/seats/:id', SeatController.updateById);
+router.delete('/seats/:id', SeatController.deleteById);
 
 module.exports = router;
